8f340704a741c071e95d0e8482cef772c1fcc901
Erick Abraham <erickabraham63@gmail.com>
2021-02-28 09:57:04 +0200

    Firebase learn

---
+++ app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    EditText edit
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
